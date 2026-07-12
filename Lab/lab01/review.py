faef115d648ae60d4ba0cdd9723b8a3c9d07c244
18639710721 <2907568338@qq.com>
2026-07-12 20:39:29 +0800

    feat: finish Lab1: Functions, Control

---
+++ Lab/lab01/review.py
@@ -0,0 +1,25 @@
+"""
+
+
+"""
+
+def welcome():
+    print('Go')
+    return 'hello'
+
+def cal():
+    print('Bears')
+    return 'world'
+
+
+# doctests
+def foo(x):
+    """A random function.
+
+    >>> foo(4)
+    4
+    >>> foo(5)
+    5
+    """
+
+
